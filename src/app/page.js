a466d9fa5b3b93fb5581baa3795a062ee3408389
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-23 18:48:18 +0300

    added HeaderSearch

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,10 @@
-import Image from 'next/image'
+import { HeaderSearch } from '@/components/HeaderSearch'
 import styles from './page.module.css'
 
 export default function Home() {
-  return <div></div>
+  return (
+    <div className={styles.App}>
+      <HeaderSearch />
+    </div>
+  )
 }
