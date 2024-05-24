202512a4dd43a251574701b97abd0ae065d2f084
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-24 11:47:21 +0300

    added data

---
+++ src/data/data.js
@@ -0,0 +1,19 @@
+import BasicCS from './BasicCSdata'
+import HTTPData from './HTTPData'
+import HTMLData from './HTMLData'
+import CSSData from './CSSData'
+import JSCoreData from './JSCoreData'
+import ReactData from './ReactData'
+import TypescriptData from './TypescriptData'
+
+const data = {
+  ...BasicCS,
+  ...HTTPData,
+  ...HTMLData,
+  ...CSSData,
+  ...JSCoreData,
+  ...ReactData,
+  ...TypescriptData,
+}
+
+export default data
