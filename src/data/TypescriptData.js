03906f396dd075ca470f674ff084ac9a42e340ac
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-24 11:47:16 +0300

    added Typescript data

---
+++ src/data/TypescriptData.js
@@ -0,0 +1,18 @@
+const Typescript = {
+  Typescript: {
+    Типы: {
+      id: '71',
+    },
+    Дженерики: {
+      id: '72',
+    },
+    Классы: {
+      id: '73',
+    },
+    'Типы Утилит(Utility Types)': {
+      id: '73',
+    },
+  },
+}
+
+export default Typescript
