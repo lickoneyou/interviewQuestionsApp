9355212086acf95a8ac3996d07c43773fc2a61df
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-24 11:46:43 +0300

    added HTTP data

---
+++ src/data/HTTPData.js
@@ -0,0 +1,30 @@
+const HTTP = {
+  HTTP: {
+    'HTTP basics': {
+      id: '21',
+    },
+    'Методы HTTP запроса': {
+      id: '22',
+    },
+    'Коды ответа': {
+      id: '23',
+    },
+    'HTTP-кеширование': {
+      id: '24',
+    },
+    HTTPS: {
+      id: '25',
+    },
+    CORS: {
+      id: '26',
+    },
+    WebSocket: {
+      id: '27',
+    },
+    'HTTP-куки': {
+      id: '28',
+    },
+  },
+}
+
+export default HTTP
