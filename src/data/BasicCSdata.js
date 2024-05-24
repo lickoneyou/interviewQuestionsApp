d9d3552e6164aebc8696e35dabca4956594a44bd
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-24 11:46:08 +0300

    added BasicCS

---
+++ src/data/BasicCSdata.js
@@ -0,0 +1,18 @@
+const BasicCS = {
+  'Basic CS principles': {
+    'Система контроля версий': {
+      id: '11',
+    },
+    'Bash: Основы командной строки': {
+      id: '12',
+    },
+    'Основные принципы ООП': {
+      id: '13',
+    },
+    SOLID: {
+      id: '14',
+    },
+  },
+}
+
+export default BasicCS
