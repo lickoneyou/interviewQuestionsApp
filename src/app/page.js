7a1980d6710504ecb87f5be46d0d963c881f88b7
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-24 11:44:38 +0300

    added AccordionFAQ

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
-import { HeaderSearch } from '@/components/HeaderSearch'
+import { AccordionFAQ } from '@/components/Accordion/AccordionFAQ'
+import { HeaderSearch } from '@/components/HeaderSearch/HeaderSearch'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <div className={styles.App}>
       <HeaderSearch />
+      <AccordionFAQ />
     </div>
   )
 }
