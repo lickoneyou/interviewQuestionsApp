03959fb4dec8b344bf15a1cc94877d2b49bbe923
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-05-26 11:30:17 +0300

    added 'Типы' data

--- src/data/TypescriptData.js
+++ src/data/TypescriptData.js
@@ -2,6 +2,92 @@ const Typescript = {
   Typescript: {
     Типы: {
       id: '71',
+      jsx: (
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Тип</th>
+                <th>Значение</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <b>boolean</b>
+                </td>
+                <td>для определения булевого значения</td>
+              </tr>
+              <tr>
+                <td>
+                  <b>number</b>
+                </td>
+                <td>определяет число</td>
+              </tr>
+              <tr>
+                <td>
+                  <b>string</b>
+                </td>
+                <td>определяет строку</td>
+              </tr>
+              <tr>
+                <td>
+                  <b>null</b>
+                </td>
+                <td>определяет null</td>
+              </tr>
+              <tr>
+                <td>
+                  <b>undefined</b>
+                </td>
+                <td>определяет undefined</td>
+              </tr>
+              <tr>
+                <td>
+                  <b>void</b>
+                </td>
+                <td>для определения отсутствующих типов</td>
+              </tr>
+              <tr>
+                <td>
+                  <b>number[]</b>
+                </td>
+                <td>для определения массива чисел</td>
+              </tr>
+              <tr>
+                <td>
+                  <b>any</b>
+                </td>
+                <td>все что угодно</td>
+              </tr>
+              <tr>
+                <td>
+                  <b>ENUM</b>
+                </td>
+                <td>для создания именованных констант</td>
+              </tr>
+              <tr>
+                <td>
+                  <b>never</b>
+                </td>
+                <td>функция вызывает ошибку, бесконечный цикл</td>
+              </tr>
+              <tr>
+                <td>
+                  <b>object</b>
+                </td>
+                <td>для обозначения объекта</td>
+              </tr>
+              <tr>
+                <td>
+                  <b>type</b>
+                </td>
+                <td>для создания пользовательского типа</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      ),
     },
     Дженерики: {
       id: '72',
