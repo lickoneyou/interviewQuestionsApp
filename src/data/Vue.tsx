1155aa8d2068d48d5a07a9d3e55873a30893b220
Kiryl Khomchanka <dickdurex265@gmail.com>
2026-01-04 19:21:06 +0300

    added 'Отличия между Options API и Composition API'

--- src/data/Vue.tsx
+++ src/data/Vue.tsx
@@ -72,6 +72,152 @@ const Vue = {
         </div>
       ),
     },
+    'Отличия между Options API и Composition API': {
+      id: '92',
+      title: 'Отличия между Options API и Composition API',
+      jsx: (
+        <div>
+          <p>Options API (классический подход):</p>
+          <pre>
+            <div className='numbers'>
+              <code className='codeNumber'>1</code>
+              <code className='codeNumber'>2</code>
+              <code className='codeNumber'>3</code>
+              <code className='codeNumber'>4</code>
+              <code className='codeNumber'>5</code>
+              <code className='codeNumber'>6</code>
+              <code className='codeNumber'>7</code>
+              <code className='codeNumber'>8</code>
+              <code className='codeNumber'>9</code>
+              <code className='codeNumber'>10</code>
+              <code className='codeNumber'>11</code>
+              <code className='codeNumber'>12</code>
+              <code className='codeNumber'>13</code>
+            </div>
+            <code>
+              <code>{'<template>'}</code>
+              <code>
+                {'  '}
+                {'<button @click="count++">Кликнули {{ count }} раз</button>'}
+              </code>
+              <code>{'</template>'}</code>
+              <code>{'  '}</code>
+              <code>{'<script>'}</code>
+              <code>export default {'{'}</code>
+              <code>
+                {'  '}data() {'{'}
+              </code>
+              <code>
+                {'    '}return {'{'}
+              </code>
+              <code>{'      '}count: 0</code>
+              <code>
+                {'    '}
+                {'{'}
+              </code>
+              <code>
+                {'  '}
+                {'{'}
+              </code>
+              <code>{'}'}</code>
+              <code>{'</script>'}</code>
+            </code>
+          </pre>
+          <p>Composition API (новый подход)</p>
+          <pre>
+            <div className='numbers'>
+              <code className='codeNumber'>1</code>
+              <code className='codeNumber'>2</code>
+              <code className='codeNumber'>3</code>
+              <code className='codeNumber'>4</code>
+              <code className='codeNumber'>5</code>
+              <code className='codeNumber'>6</code>
+              <code className='codeNumber'>7</code>
+              <code className='codeNumber'>8</code>
+              <code className='codeNumber'>9</code>
+            </div>
+            <code>
+              <code>{'<template>'}</code>
+              <code>
+                {'  '}
+                {'<button @click="count++">Кликнули {{ count }} раз</button>'}
+              </code>
+              <code>{'</template>'}</code>
+              <code>{'  '}</code>
+              <code>{'<script setup>'}</code>
+              <code>import {'{ ref }'} from 'vue'</code>
+              <code>{'  '}</code>
+              <code>const count = ref(0)</code>
+              <code>{'</script>'}</code>
+            </code>
+          </pre>
+          <p>Ключевые отличия:</p>
+          <ul>
+            <li>
+              Организация кода:
+              <ul>
+                <li>
+                  <b>Options API:</b> Код организован по опциям (data, methods,
+                  computed и т.д.)
+                </li>
+                <li>
+                  <b>Composition API:</b> Код организован по логической
+                  функциональности (все связанное с одной логикой вместе)
+                </li>
+              </ul>
+            </li>
+            <li>
+              Переиспользование логики:
+              <ul>
+                <li>
+                  <b>Options API:</b> Mixins, которые имеют проблемы с
+                  конфликтами имен и неявными зависимостями
+                </li>
+                <li>
+                  <b>Composition API:</b> Композаблы (composables) - чистые
+                  функции JavaScript
+                </li>
+              </ul>
+            </li>
+            <li>
+              Типизация:
+              <ul>
+                <li>
+                  <b>Options API:</b> Ограниченная поддержка TypeScript
+                </li>
+                <li>
+                  <b>Composition API:</b> Отличная поддержка TypeScript
+                </li>
+              </ul>
+            </li>
+            <li>
+              this контекст:
+              <ul>
+                <li>
+                  <b>Options API:</b> Используется <b>this</b> для доступа к
+                  свойствам
+                </li>
+                <li>
+                  <b>Composition API:</b> Нет <b>this</b>, используются refs и
+                  reactive
+                </li>
+              </ul>
+            </li>
+            <li>
+              Гибкость:
+              <ul>
+                <li>
+                  <b>Options API:</b> Более строгая структура
+                </li>
+                <li>
+                  <b>Composition API:</b> Больше свободы в организации кода
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      ),
+    },
   },
 };
 
