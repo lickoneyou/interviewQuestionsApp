3cf1eff25418cb85e7e6e7f360dc5ae24451f55c
Kiryl Khomchanka <dickdurex265@gmail.com>
2026-06-24 16:14:46 +0300

    added course page

--- src/app/(pages)/[course]/page.tsx
+++ src/app/(pages)/[course]/page.tsx
@@ -1,5 +1,17 @@
+import React from 'react';
+
+import { HeaderSearch } from '../../../components/HeaderSearch/HeaderSearch';
+import CourseTopics from '../../../components/CourseTopics/CourseTopics';
+
+import styles from '../../page.module.css'
+
 const Course = () => {
+  return (
+    <div className={styles.App}>
+      <HeaderSearch />
+      <CourseTopics />
+    </div>
+  );
+};
 
-}
-
-export default Course
+export default Course;
---
+++ src/components/CourseTopics/CourseTopics.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+import { useParams } from 'next/navigation';
+import slugifyText from '../../handlers/slugifyText';
+
+import styles from './CourseTopict.module.css';
+
+import data from '../../data/data';
+
+import type { Data, FullData } from '../../types';
+
+const CourseTopics = () => {
+  const params = useParams();
+
+  const chapters = Object.keys(data as FullData);
+
+  return (
+    <main className={styles.container}>
+      {Array.isArray(chapters) &&
+        chapters.map((chapter) => {
+          if (slugifyText(chapter) === params.course) {
+            const ch: Data = (data as FullData)[chapter];
+
+            return Object.keys(ch).map((title, index) => {
+              return (
+                <Link
+                  className={styles.links}
+                  key={`${index}-${slugifyText(title)}`}
+                  href={`${params.course}/${slugifyText(title)}`}
+                >
+                  {title}
+                </Link>
+              );
+            });
+          }
+        })}
+    </main>
+  );
+};
+
+export default CourseTopics;
